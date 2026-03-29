29f012f5c7154f907881dacb414e521a89e26e9c
Luigi Grilli <luigi.grilli@gmail.com>
2026-03-29 11:20:26 +0100

    Simplified redme

--- src/Grillisoft.DotnetTools.NewRepo.Creators/Impl/ReadmeCreator.cs
+++ src/Grillisoft.DotnetTools.NewRepo.Creators/Impl/ReadmeCreator.cs
@@ -1,8 +1,5 @@
-﻿using System;
 using Grillisoft.DotnetTools.NewRepo.Abstractions;
 using Microsoft.Extensions.Logging;
-using System.IO.Abstractions;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,62 +7,29 @@ namespace Grillisoft.DotnetTools.NewRepo.Creators.Impl
 {
     public class ReadmeCreator : CreatorBase
     {
-        public string Name = "README.md";
-
-        private readonly IHttpClientFactory _httpClientFactory;
+        public const string Name = "README.md";
 
         public ReadmeCreator(
             INewRepoSettings settings,
-            ILogger<ReadmeCreator> logger,
-            IHttpClientFactory httpClientFactory)
+            ILogger<ReadmeCreator> logger)
             : base(settings, logger)
         {
-            _httpClientFactory = httpClientFactory;
         }
-
-        public string Url => $"https://raw.githubusercontent.com/othneildrew/Best-README-Template/master/BLANK_README.md";
 
         public override async Task Create(CancellationToken cancellationToken)
         {
-            if(_settings.EmptyReadme)
+            if (_settings.EmptyReadme)
             {
-                await this.CreateTextFile(this.Root.File(Name), $"##{_settings.Name}");
+                await this.CreateTextFile(this.Root.File(Name), $"# {_settings.Name}");
                 return;
             }
 
-            var responseBody = await DownloadReadme(cancellationToken);
-            if (string.IsNullOrWhiteSpace(responseBody))
-                return;
-            
-            //responseBody = responseBody.Replace("github_username", _settings.GithubUsername);
-            //responseBody = responseBody.Replace("repo_name", _settings.GithubRepoName);
-            responseBody = responseBody.Replace("twitter_handle", _settings.TwitterUsername);
-            responseBody = responseBody.Replace("project_title", _settings.Name);
-            responseBody = responseBody.Replace("project_description", _settings.Product);
+            var content = (await GetTemplateContent(Name))
+                .Replace("project_name", _settings.Name)
+                .Replace("project_description", _settings.Product)
+                .Replace("project_license", _settings.License);
 
-            var index = responseBody.IndexOf("-->", StringComparison.Ordinal);
-            if (index > 0)
-                responseBody = responseBody.Substring(index + 3);
-
-            await this.CreateTextFile(this.Root.File(Name), responseBody);
-        }
-
-        private async Task<string> DownloadReadme(CancellationToken cancellationToken)
-        {
-            try
-            {
-                using var client = _httpClientFactory.CreateClient();
-                _logger.LogInformation("Downloading {0} from {1}", this.Name, this.Url);
-                var response = await client.GetAsync(this.Url, cancellationToken);
-                response.EnsureSuccessStatusCode();
-
-                return await response.Content.ReadAsStringAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to download {Name}. The readme file will not be created", this.Name);
-                return string.Empty;
-            }
+            await this.CreateTextFile(this.Root.File(Name), content);
         }
     }
 }
---
+++ src/Grillisoft.DotnetTools.NewRepo.Creators/Templates/README.md
@@ -0,0 +1,15 @@
+# project_name
+
+project_description
+
+## Getting Started
+
+### Installation
+
+```sh
+dotnet add package project_name
+```
+
+## License
+
+Distributed under the project_license License. See `LICENSE` for more information.
